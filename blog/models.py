55c7536d2ca9defac15ba059f954ff324e5c1878
Kevs <k.vashhenko@gmai.com>
2021-09-21 21:12:18 +0300

    update admin

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-'''
-Category
-=========
-title, slug
-
-Tag
-=========
-title, slug
-
-Post
-=========
-title, slug, author, content, created_at, photo, views, category, tags
-'''
 
 
 class Category(models.Model):
@@ -49,15 +36,15 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
-    author = models.CharField(max_length=100)
-    content = models.TextField(blank=True)
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    content = models.TextField(blank=True, verbose_name='Статья')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
     views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
-    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категории')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts', verbose_name='Тэги')
 
     def __str__(self):
         return self.title
